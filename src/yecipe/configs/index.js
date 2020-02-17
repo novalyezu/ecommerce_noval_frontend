0abff72d6818c876baebbc2b1a6e251655f5dafe
noval <novalyezoe@gmail.com>
2020-02-17 08:19:36 +0700

    deploy

--- src/yecipe/configs/index.js
+++ src/yecipe/configs/index.js
@@ -1,5 +1,6 @@
 const config = {
-  url_dev: "http://localhost:3000/api/v1"
+  url_prod: "http://localhost:3000/api/v1",
+  url_dev: "https://novalcommerce.herokuapp.com/api/v1"
 };
 
 export default config;
